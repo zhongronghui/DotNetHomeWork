6fc6cfd6d111c6ec3fda25079abb7a11a7b3db04
zhongronghui <80029184+zhongronghui@users.noreply.github.com>
2021-04-29 21:28:45 +0800

    Add files via upload

---
+++ homework1/Work1/Work1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个数字：");
+            String A = Console.ReadLine();
+            Console.Write("输入运算符+，-，*，/其中一个：");
+            String B = Console.ReadLine();
+            Console.Write("请输入第2个数字：");
+            String C = Console.ReadLine();
+            Double a, c;
+            a = Convert.ToDouble(A);
+            c = Convert.ToDouble(C);
+            string D="";
+            if (B == "+")
+            { D = Convert.ToString(a + c); }
+            else if (B == "-")
+            { D = Convert.ToString(a - c); }
+            else if (B == "*")
+            { D = Convert.ToString(a * c); }
+            else if (B == "/" && c != 0)
+            {
+                D = Convert.ToString(a / c);
+            }
+            else
+            {
+                Console.WriteLine("分母不能为零");
+            }
+
+            Console.WriteLine(D);
+
+        }
+    }
+}
---
+++ homework1/Work2/Work2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Double a, b;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            if (comboBox1.Text == "+")
+            { textBox3.Text = Convert.ToString(a + b); }
+
+            else if (comboBox1.Text == "-")
+            { textBox3.Text = Convert.ToString(a - b); }
+            else if (comboBox1.Text == "*")
+            { textBox3.Text = Convert.ToString(a * b); }
+            else
+            if (comboBox1.Text == "/" && b != 0)
+            { textBox3.Text = Convert.ToString(a / b); 
+            }
+            else
+            {
+                textBox3.Text = "除数不能为0";
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
