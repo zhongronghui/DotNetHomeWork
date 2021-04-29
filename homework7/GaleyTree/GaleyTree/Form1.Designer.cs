9d40ca2fbea20ed9b4e1e800557891add8b560ec
zhongronghui <80029184+zhongronghui@users.noreply.github.com>
2021-04-29 21:52:57 +0800

    Add files via upload

---
+++ homework7/GaleyTree/GaleyTree/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace GaleyTree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rightPer1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.leftPer2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rightTh1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.leftTh2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(358, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(381, 418);
+            this.panel2.TabIndex = 0;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "递归深度（n）";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(207, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(109, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "主干长度（leng）";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(207, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(109, 25);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "右分支长度比(per1)";
+            // 
+            // rightPer1
+            // 
+            this.rightPer1.Location = new System.Drawing.Point(207, 133);
+            this.rightPer1.Name = "rightPer1";
+            this.rightPer1.Size = new System.Drawing.Size(109, 25);
+            this.rightPer1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "左分支长度比(per2)";
+            // 
+            // leftPer2
+            // 
+            this.leftPer2.Location = new System.Drawing.Point(207, 164);
+            this.leftPer2.Name = "leftPer2";
+            this.leftPer2.Size = new System.Drawing.Size(109, 25);
+            this.leftPer2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "右分支角度(th1)";
+            // 
+            // rightTh1
+            // 
+            this.rightTh1.Location = new System.Drawing.Point(207, 195);
+            this.rightTh1.Name = "rightTh1";
+            this.rightTh1.Size = new System.Drawing.Size(109, 25);
+            this.rightTh1.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 15);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "左分支角度(th2)";
+            // 
+            // leftTh2
+            // 
+            this.leftTh2.Location = new System.Drawing.Point(207, 226);
+            this.leftTh2.Name = "leftTh2";
+            this.leftTh2.Size = new System.Drawing.Size(109, 25);
+            this.leftTh2.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(123, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "颜色选择(color)";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(207, 257);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 23);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(764, 442);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.leftTh2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.rightTh1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.leftPer2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rightPer1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox rightPer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox leftPer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox rightTh1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox leftTh2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ homework7/GaleyTree/GaleyTree/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaleyTree
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        Pen pen = Pens.Black;
+        int n = 10;
+        int length = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "10";
+            textBox2.Text = "100 ";
+            rightPer1.Text = "0.6";
+            leftPer2.Text = "0.7";
+            rightTh1.Text = "0.521";
+            leftTh2.Text = "0.345";
+            comboBox1.Items.Add("绿色");
+            comboBox1.Items.Add("红色");
+            comboBox1.Items.Add("蓝色");
+            comboBox1.Items.Add("灰色");      
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (graphics != null) graphics.Clear(panel2.BackColor);
+            try
+            {   n = Convert.ToInt32(textBox1.Text);
+                length = Convert.ToInt32(textBox2.Text);
+                per1 = Convert.ToDouble(rightPer1.Text);
+                per2 = Convert.ToDouble(leftPer2.Text);
+                th1 = Convert.ToDouble(rightTh1.Text);
+                th2 = Convert.ToDouble(leftTh2.Text);
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("请输入正确数值");
+            }
+
+            if (graphics == null) graphics = panel2.CreateGraphics();
+                drawGayleyTree(n, 200, 300, length, -Math.PI / 2); 
+        }
+       
+       
+
+        void drawGayleyTree(int n ,double x0, double y0 ,double leng,double th)
+        {
+            if (n == 0) 
+                return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+            drawGayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawGayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+
+        }
+       
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+          
+
+            switch (comboBox1.Text)
+            {
+                case "绿色" : pen=Pens.Green;  break;
+                case "红色" : pen = Pens.Red;  break;
+                case "蓝色" : pen = Pens.Blue; break;
+                case "灰色" :pen = Pens.Gray;break;
+                default: pen = Pens.Blue; break;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
